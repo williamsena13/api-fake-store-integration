aab76cdde0c5bc935b015b43271c1402330d17fd
William Basseto Sena <williamsena13@gmail.com>
2025-08-23 12:10:30 -0300

    Incluso rotina de atualização automatica

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('fakestore:sync --mode=delta')
+                 ->dailyAt('02:00')
+                 ->withoutOverlapping()
+                 ->runInBackground();
     }
 
     /**
